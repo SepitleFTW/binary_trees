4537748d5e97bbf811748d2714cfed7f295a357b
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-02-29 21:58:07 +0200

    ..

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,18 +9,20 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	/* If node is NULL or has no parent or grandparent, it has no uncle */
+	/** If node is NULL or has no parent
+     * or grandparent, it has no uncle
+     */
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
 	/** If parent's parent's left child is
-    *the parent, return right child as uncle
+    * the parent, return right child as uncle
     */
 	if (node->parent->parent->left == node->parent)
 		return (node->parent->parent->right);
 
-	/* If parent's parent's right child is the parent,
-    *return left child as uncle
+	/** If parent's parent's right child is the parent,
+    * return left child as uncle
     */
 	return (node->parent->parent->left);
 }
