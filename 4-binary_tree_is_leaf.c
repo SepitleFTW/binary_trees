e6ac63a70c5838ac4759ab2a15a5cc97c8e9b20d
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-02-29 20:36:14 +0200

    ...

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -10,15 +10,18 @@
 */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (node == NULL)
+    {if (node == NULL)
     return (0);
+    }
 
-    if (node->left != NULL)
+    {if (node->left != NULL)
     return (0);
+    }
 
-    if (node->right!= NULL)
+    {if (node->right!= NULL)
     return (0);
+    }
 
-    
+
     return (1);
 }
