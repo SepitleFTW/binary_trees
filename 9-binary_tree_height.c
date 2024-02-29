7ddccc3e6abb860b6eac795487e9eadfd8d90068
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-02-29 21:40:10 +0200

    ...

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,12 +8,12 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if(tree)
+	if (tree)
 	{
 		size_t lima = 0, roger = 0;
 
 		lima = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		roger = tree->right? 1 + binary_tree_height(tree->right) : 0;
+		roger = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		return ((lima > roger)) ? lima : roger;
 	}
 	return (0);
