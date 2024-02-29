34b9c249cbdf27b2d27d19c81eac7d048bc36137
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-02-29 21:36:39 +0200

    ...

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -6,7 +6,7 @@
  *@tree: Pointer to the root node of the tree to traverse
  *@func: Pointer to a function to call for each node
  */
-void binary_tree_preorder(const binary_tree_t *tree, void (*fun)(int))
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree && func)
 	{
