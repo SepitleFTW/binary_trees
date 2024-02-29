5011cf843105ad9d60b3f99e768c8015e91dd801
SepitleFTW <sepitleleshilo69@gmail.com>
2024-02-29 11:29:19 -0800

    ,.,

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -5,13 +5,13 @@
  * in-order traversal
  * @tree: pointer to the root node of tree
  * @func: pointer to function to call for node
-*/
+ */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree && func)
-    {
-        binary_tree_inorder(tree->left, func);
-        func(tree->n);
-        binary_tree_inorder(tree->right, func);
-    }
+	if (tree && func)
+	{
+		binary_tree_inorder(tree->left, func);
+		func(tree->n);
+		binary_tree_inorder(tree->right, func);
+	}
 }
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -5,16 +5,16 @@
  * @tree: pointer to the root of the tree
  * Return: if tree == NULL return 0
  * else return height
-*/
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    if(tree)
-    {
-        size_t lima = 0, roger = 0;
+	if(tree)
+	{
+		size_t lima = 0, roger = 0;
 
-        lima = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-        roger = tree->right? 1 + binary_tree_height(tree->right) : 0;
-        return ((lima > roger)) ? lima : roger;
-    }
-    return (0);
+		lima = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		roger = tree->right? 1 + binary_tree_height(tree->right) : 0;
+		return ((lima > roger)) ? lima : roger;
+	}
+	return (0);
 }
