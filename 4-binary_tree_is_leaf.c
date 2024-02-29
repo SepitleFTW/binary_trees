19b3429a07bd6c129ee49a6306159c7f9e64ca09
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-02-29 20:40:25 +0200

    updated taskk 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,23 +1,19 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - this checks if a node
- * is a leaf od the binary tree
- * @node: this is a pointer to the node that
- * must be checked
- * Return: 1 if is leaf
- * else return 0
-*/
+ * binary_tree_is_leaf - Checks if a node is
+ * a leaf of the binary tree
+ * @node: Pointer to the node to be checked
+ *
+ * Return: 1 if the node is a leaf, 0 otherwise
+ */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (node == NULL)
-    return (0);
+	if (node == NULL)
+		return (0);
 
-    if (node->left != NULL)
-    return (0);
+	if (node->left != NULL || node->right != NULL)
+		return (0);
 
-    if (node->right!= NULL)
-    return (0);
-
-    return (1);
+	return (1);
 }
